a8cf56d73cccfb0a5f3f121fb0bcdeb571891335
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-08-08 17:09:01 -0300

    feat: add test

---
+++ src/test/java/com/matheus/api_auto_report/ApiAutoReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheus.api_auto_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAutoReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
