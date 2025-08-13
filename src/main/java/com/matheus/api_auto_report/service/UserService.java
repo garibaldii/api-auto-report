7621f4081acc61e71b59eff8d112bf07b60e08ec
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-08-13 11:33:22 -0300

    feat: add UserService

--- src/main/java/com/matheus/api_auto_report/service/UserService.java
+++ src/main/java/com/matheus/api_auto_report/service/UserService.java
@@ -1,5 +1,43 @@
 package com.matheus.api_auto_report.service;
 
+import com.matheus.api_auto_report.persistence.dao.UserDAO;
+import com.matheus.api_auto_report.persistence.entity.UserEntity;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Service
+@AllArgsConstructor
 public class UserService {
 
+    @Autowired
+    private final DataSource dataSource;
+
+    private final PasswordEncoder encoder;
+
+    public UserEntity insert(UserEntity entity) throws SQLException {
+        Connection connection = dataSource.getConnection();
+        try {
+            connection.setAutoCommit(false);
+            UserDAO dao = new UserDAO(connection);
+
+            String hashedPassword = encoder.encode(entity.getPassword());
+            entity.setPassword(hashedPassword);
+            dao.insert(entity);
+
+            connection.commit();
+
+            return entity;
+        } catch (SQLException e) {
+            connection.rollback();
+            connection.close();
+            e.printStackTrace();
+            throw e;
+        }
+    }
 }
