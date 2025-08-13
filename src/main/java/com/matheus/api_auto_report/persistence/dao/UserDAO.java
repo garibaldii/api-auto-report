d4a6c7c6c54c19014e88151de1a2d954d5a83465
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-08-13 11:33:06 -0300

    feat: add UserDAO

---
+++ src/main/java/com/matheus/api_auto_report/persistence/dao/UserDAO.java
@@ -0,0 +1,37 @@
+package com.matheus.api_auto_report.persistence.dao;
+
+import com.matheus.api_auto_report.persistence.entity.UserEntity;
+
+import javax.sql.DataSource;
+import java.sql.*;
+
+import com.mysql.cj.jdbc.StatementImpl;
+
+public class UserDAO {
+
+    private final Connection connection;
+
+    public UserDAO(Connection connection){
+        this.connection = connection;
+    }
+
+    public UserEntity insert(final UserEntity entity) throws SQLException {
+        var sql = "INSERT INTO USERS (name, email, hashed_password) VALUES (?, ?, ?)";
+        try (PreparedStatement statement = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            statement.setString(1, entity.getName());
+            statement.setString(2, entity.getEmail());
+            statement.setString(3, entity.getPassword());
+
+            statement.executeUpdate();
+
+            try (ResultSet keys = statement.getGeneratedKeys()) {
+                if (keys.next()) {
+                    entity.setId(keys.getLong(1)); // Coluna 1 = id gerado
+                }
+            }
+        }
+
+        return entity;
+    }
+
+}
