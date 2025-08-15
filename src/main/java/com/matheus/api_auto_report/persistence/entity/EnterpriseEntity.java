67a4afa5d06dfc34286d9e7788cd33f4c4154021
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-08-15 15:12:37 -0300

    feat: add input validation to user and enterprise entities

--- src/main/java/com/matheus/api_auto_report/persistence/entity/EnterpriseEntity.java
+++ src/main/java/com/matheus/api_auto_report/persistence/entity/EnterpriseEntity.java
@@ -1,8 +1,13 @@
 package com.matheus.api_auto_report.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.hibernate.validator.constraints.UniqueElements;
+import org.hibernate.validator.constraints.br.CNPJ;
 
 import java.util.List;
 
@@ -14,8 +19,17 @@ public class EnterpriseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
+    @Size(max = 50)
     private String name;
+
+    @CNPJ
+    @NotBlank
     private String cnpj;
+
+    @NotBlank
+    @UniqueElements
+    @Size(max = 20)
     private String cellphoneNumber;
 
     @ManyToOne
--- src/main/java/com/matheus/api_auto_report/persistence/entity/UserEntity.java
+++ src/main/java/com/matheus/api_auto_report/persistence/entity/UserEntity.java
@@ -1,6 +1,9 @@
 package com.matheus.api_auto_report.persistence.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -17,8 +20,16 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Field can not be blank")
+    @Size(max = 50, message = "Name can not be longer than 50 characters")
     private String name;
+
+    @NotBlank(message = "Field can not be blank")
+    @Email(message = "E-mail invalid")
     private String email;
+
+    @NotBlank(message = "Field can not be blank")
+    @Size(max = 244, message = "Password can not be longer than 244 characters")
     private String password;
 
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
