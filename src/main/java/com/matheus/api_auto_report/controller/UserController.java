cb82fabce5c67dd0d219052200602c147e99cb53
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-08-15 15:14:43 -0300

    refactor: add @valid notation to enable customized exceptions messages.

--- src/main/java/com/matheus/api_auto_report/controller/UserController.java
+++ src/main/java/com/matheus/api_auto_report/controller/UserController.java
@@ -2,11 +2,15 @@ package com.matheus.api_auto_report.controller;
 
 import com.matheus.api_auto_report.persistence.entity.UserEntity;
 import com.matheus.api_auto_report.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/user")
@@ -16,9 +20,11 @@ public class UserController {
     private final UserService service;
 
     @PostMapping
-    public UserEntity createUser(@RequestBody UserEntity user) throws Exception {
+    public UserEntity createUser(@RequestBody @Valid UserEntity user) throws Exception {
         return service.insert(user);
     }
+
+
 
 }
 
