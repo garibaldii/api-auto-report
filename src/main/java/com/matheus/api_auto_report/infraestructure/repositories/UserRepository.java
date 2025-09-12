4a6f47c51f03a5c55456d80831870e21097aada5
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-12 16:52:11 -0300

    feat: add UserRepository

---
+++ src/main/java/com/matheus/api_auto_report/infraestructure/repositories/UserRepository.java
@@ -0,0 +1,14 @@
+package com.matheus.api_auto_report.infraestructure.repositories;
+
+import com.matheus.api_auto_report.infraestructure.dto.UserResponseDTO;
+import com.matheus.api_auto_report.infraestructure.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+
+    Optional<UserEntity> findByEmail(String email);
+
+    void deleteByEmail(String email);
+}
