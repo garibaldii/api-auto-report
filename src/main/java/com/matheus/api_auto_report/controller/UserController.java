3a57acef7f3684a9225f58085187d4ec38c4f71f
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-16 16:47:56 -0300

    refactor: move jakarta validation to dto

--- src/main/java/com/matheus/api_auto_report/controller/UserController.java
+++ src/main/java/com/matheus/api_auto_report/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.matheus.api_auto_report.controller;
 
+import com.matheus.api_auto_report.infraestructure.dto.UserRequestDTO;
+import com.matheus.api_auto_report.infraestructure.dto.UserResponseDTO;
 import com.matheus.api_auto_report.infraestructure.entity.UserEntity;
 import com.matheus.api_auto_report.business.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/user")
@@ -16,24 +19,29 @@ public class UserController {
 
     private final UserService service;
 
-    @PostMapping
-    public UserEntity createUser(@RequestBody @Valid UserEntity user) throws Exception {
-        return service.insert(user);
+    @GetMapping("/{id}")
+    public ResponseEntity<UserResponseDTO> getUserById(@PathVariable Long id) {
+        UserResponseDTO dto = service.findUserById(id);
+
+        return ResponseEntity.ok(dto);
     }
 
-    @GetMapping
-    public Optional<UserDTO> getUser(@RequestParam int id) throws Exception {
-        return service.findById(id);
+    @PostMapping("/")
+    public UserEntity postUser(@Valid @RequestBody UserRequestDTO dto) {
+
+        return service.saveUser(dto);
+
     }
 
-    @GetMapping
-    public List<UserDTO> getUsers() throws Exception {
-        return service.getUsers();
+    //notacao usada para atualizacao parcial de objeto
+    @PatchMapping("/{email}")
+    public void patchUser(@PathVariable String email, @RequestBody UserRequestDTO dto) {
+        service.updateUserByEmail(email, dto);
     }
 
-    @DeleteMapping
-    public boolean deleteUser(@RequestParam  long id) throws Exception {
-        return service.deleteById(id);
+    @DeleteMapping("/{email}")
+    public void deleteUser(@PathVariable String email) {
+        service.deleteUserByEmail(email);
     }
 
 
