f419aaf3932458ff170252b614f96b024164ffca
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-18 16:34:50 -0300

    feat: add query methods for groups

--- src/main/java/com/matheus/api_auto_report/infraestructure/repositories/BussinessGroupRepository.java
+++ src/main/java/com/matheus/api_auto_report/infraestructure/repositories/BussinessGroupRepository.java
@@ -3,6 +3,11 @@ package com.matheus.api_auto_report.infraestructure.repositories;
 import com.matheus.api_auto_report.infraestructure.entity.BussinessGroupEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface BussinessGroupRepository extends JpaRepository<BussinessGroupEntity, Long> {
+import java.util.List;
+import java.util.Optional;
 
+public interface BussinessGroupRepository extends JpaRepository<BussinessGroupEntity, Long> {
+    List<BussinessGroupEntity> findAllByUser_Id(Long userId);
+
+    Optional<BussinessGroupEntity> findByIdAndUser_Id(Long groupId, Long userId);
 }
