a0139ffe7d6bd974f51fdf1c91a7f5158b1793d2
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-18 16:32:58 -0300

    refactor: switch method calling to a more indicated one

--- src/main/java/com/matheus/api_auto_report/business/validator/UserValidator.java
+++ src/main/java/com/matheus/api_auto_report/business/validator/UserValidator.java
@@ -1,28 +1,25 @@
 package com.matheus.api_auto_report.business.validator;
 
-import com.matheus.api_auto_report.exception.exs.DuplicateData;
+import com.matheus.api_auto_report.exception.exs.DuplicatedData;
 import com.matheus.api_auto_report.exception.exs.NotFound;
-import com.matheus.api_auto_report.infraestructure.dto.UserRequestDTO;
 import com.matheus.api_auto_report.infraestructure.repositories.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class UserValidator {
 
     private final UserRepository repository;
 
-    public UserValidator(UserRepository repository) {
-        this.repository = repository;
-    }
-
     public void verifyUniqueEmail(String email) {
         if (repository.findByEmail(email).isPresent()) {
-            throw new DuplicateData("E-mail already registered");
+            throw new DuplicatedData("E-mail already registered");
         }
     }
 
     public void verifyUserExistsById(Long id) {
-        if (repository.findById(id).isEmpty()) {
+        if (!repository.existsById(id)) {
             throw new NotFound("User not found");
         }
     }
