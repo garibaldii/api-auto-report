40fc9dd891045d0db55d0c2ee49f3f1fc9d24b03
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-18 16:39:30 -0300

    refactor: find out a new way to reference user and group without user's data leak

--- src/main/java/com/matheus/api_auto_report/business/service/BussinessGroupService.java
+++ src/main/java/com/matheus/api_auto_report/business/service/BussinessGroupService.java
@@ -4,8 +4,8 @@ import com.matheus.api_auto_report.business.validator.BussinessGroupValidator;
 import com.matheus.api_auto_report.exception.exs.NotFound;
 import com.matheus.api_auto_report.infraestructure.entity.BussinessGroupEntity;
 import com.matheus.api_auto_report.infraestructure.entity.EnterpriseEntity;
-import com.matheus.api_auto_report.infraestructure.entity.UserEntity;
 import com.matheus.api_auto_report.infraestructure.repositories.BussinessGroupRepository;
+import com.matheus.api_auto_report.infraestructure.repositories.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -15,46 +15,68 @@ import java.util.List;
 @AllArgsConstructor
 public class BussinessGroupService {
 
-    private final UserEntity user;
-    private final BussinessGroupRepository repository;
+    private final BussinessGroupRepository groupRepository;
+
+    /* Estamos chamando o UserRepository para poder fazer a referência dos grupos com usuário sem a necessidade
+     * de carregar o objeto inteiro do usuário(evitando a vulnerabilidade de vazamento de dados).*/
+    private final UserRepository userRepository;
     private final BussinessGroupValidator validator;
 
-    public BussinessGroupEntity saveGroup(BussinessGroupEntity entity) {
-        return repository.saveAndFlush(entity);
+
+    //cria um grupo vinculado ao usuário específico
+    public BussinessGroupEntity saveGroup(BussinessGroupEntity entity, Long userId) {
+        //getReferenceById faz somente a referencia ao objeto, nao carregando todos os dados do usuário
+        entity.setUser(userRepository.getReferenceById(userId));
+        return groupRepository.saveAndFlush(entity);
     }
 
-    public List<BussinessGroupEntity> getGroups() {
-        return user.getGroups();
+    //acha todos os grupos do usuário específico
+    public List<BussinessGroupEntity> getGroups(Long userId) {
+
+        //encontra todos os grupos vinculados pelo id, funciona como se fosse essa query:
+        //SELECT * FROM bussiness_groups bg WHERE bg.user_id = ?
+        return groupRepository.findAllByUser_Id(userId);
     }
 
-    public BussinessGroupEntity getGroupById(Long id) {
-        return user.getGroups().stream().filter(
-                group -> group.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new NotFound("Cant found  group id vinculated with your user"));
+    //acha um grupo específico do usuário específico
+    //SELECT FROM bussiness_groups bg WHERE bg.user_id = ? AND bg.id = ?
+    public BussinessGroupEntity getGroupById(Long groupId, Long userId) {
+        return groupRepository.findByIdAndUser_Id(groupId, userId)
+                .orElseThrow(() -> new NotFound("Cant found related group"));
     }
 
-    public BussinessGroupEntity updateGroup(Long id, BussinessGroupEntity data) {
+
+    //UPDATE bussiness_groups
+    //SET name = ?
+    //WHERE id = ? AND user_id = ?;
+    public BussinessGroupEntity updateGroup(Long id, BussinessGroupEntity data, Long userId) {
         validator.verifyGroupExistsById(id);
 
-        BussinessGroupEntity entity = getGroupById(id);
+        BussinessGroupEntity entity = getGroupById(id, userId);
 
-        if(data.getName() != null) entity.setName(data.getName());
+        if (data.getName() != null) entity.setName(data.getName());
 
-        repository.save(entity);
+        return groupRepository.saveAndFlush(entity);
 
-        return entity;
     }
 
-    public void deleteGroup(Long id) {
+    public void deleteGroup(Long id, Long userId) {
         validator.verifyGroupExistsById(id);
 
-        BussinessGroupEntity entity = getGroupById(id);
+        BussinessGroupEntity entity = getGroupById(id, userId);
 
         //faz o delete em cascata, ou seja. Possibilita a exclusao das empresas vinculadas a este grupo.
         //Seria interessante adicionar uma confirmação de código via e-mail para possibilitar a ação (crítica).
-        repository.deleteById(id);
+        groupRepository.delete(entity);
 
+    }
+
+    public List<EnterpriseEntity> getGroupEnterprises(Long groupId, Long userId) {
+        validator.verifyGroupExistsById(groupId);
+
+        BussinessGroupEntity group = getGroupById(groupId, userId);
+
+        return group.getEnterprises();
     }
 
 
--- src/main/java/com/matheus/api_auto_report/controller/BussinessGroupController.java
+++ src/main/java/com/matheus/api_auto_report/controller/BussinessGroupController.java
@@ -1,11 +1,13 @@
 package com.matheus.api_auto_report.controller;
 
 import com.matheus.api_auto_report.business.service.BussinessGroupService;
-import com.matheus.api_auto_report.infraestructure.dto.UserResponseDTO;
+import com.matheus.api_auto_report.config.security.JwtTokenService;
+import com.matheus.api_auto_report.config.security.UserDetailsImpl;
 import com.matheus.api_auto_report.infraestructure.entity.BussinessGroupEntity;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.net.URI;
@@ -17,38 +19,50 @@ import java.util.List;
 public class BussinessGroupController {
 
     private final BussinessGroupService service;
+    private final JwtTokenService jwtTokenService;
 
     @GetMapping("/{id}")
-    public ResponseEntity<BussinessGroupEntity> getGroupById(@PathVariable Long id) {
-        BussinessGroupEntity group = service.getGroupById(id);
+    public ResponseEntity<BussinessGroupEntity> getGroupById(
+            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        BussinessGroupEntity group = service.getGroupById(id, );
 
         return ResponseEntity.ok(group);
     }
 
     @GetMapping("/")
-    public ResponseEntity<List<BussinessGroupEntity>> getGroups(){
-        List<BussinessGroupEntity> groups = service.getGroups();
+    public ResponseEntity<List<BussinessGroupEntity>> getGroups(@AuthenticationPrincipal UserDetailsImpl userDetails) {
+        List<BussinessGroupEntity> groups = service.getGroups(userDetails.getUser());
 
         return ResponseEntity.ok(groups);
     }
 
     @PostMapping("/")
-    public ResponseEntity<BussinessGroupEntity> postGroup(@Valid @RequestBody BussinessGroupEntity entity) {
-        BussinessGroupEntity group = service.saveGroup(entity);
+    public ResponseEntity<BussinessGroupEntity> postGroup(
+            @Valid @RequestBody BussinessGroupEntity entity,
+            UserDetailsImpl userDetails) {
+        BussinessGroupEntity group = service.saveGroup(entity, userDetails.getUser());
 
         URI uri = URI.create("/groups/" + group.getId());
         return ResponseEntity.created(uri).body(group);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<BussinessGroupEntity> updateGroup(@PathVariable Long id, @RequestBody BussinessGroupEntity data) {
-        BussinessGroupEntity updated = service.updateGroup(id, data);
-
+    public ResponseEntity<BussinessGroupEntity> updateGroup(
+            @PathVariable Long id,
+            @RequestBody BussinessGroupEntity data,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        BussinessGroupEntity updated = service.updateGroup(id, data, userDetails.getUser());
         return ResponseEntity.ok(updated);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteGroup(@PathVariable Long id) {
-        service.deleteGroup(id);
+    public void deleteGroup(
+            @PathVariable Long id,
+            @AuthenticationPrincipal UserDetailsImpl userDetails
+    ) {
+        service.deleteGroup(id, userDetails.getUser());
     }
 }
