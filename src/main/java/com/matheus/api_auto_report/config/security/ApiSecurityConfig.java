b7d77a394c8b06b3d4b4b5562fa3396812cb89b4
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-19 16:43:49 -0300

    refactor: change endpoint gramatic

--- src/main/java/com/matheus/api_auto_report/config/security/ApiSecurityConfig.java
+++ src/main/java/com/matheus/api_auto_report/config/security/ApiSecurityConfig.java
@@ -18,8 +18,8 @@ public class ApiSecurityConfig {
     private UserAuthenticationFilter userAuthenticationFilter;
 
     public static final String [] ENDPOINTS_WITH_AUTHENTICATION_NOT_REQUIRED = {
-            "/users/login", // Url que usaremos para fazer login
-            "/users" // Url que usaremos para criar um usuário
+            "/user/login", // Url que usaremos para fazer login
+            "/user/" // Url que usaremos para criar um usuário
     };
 
     @Bean
