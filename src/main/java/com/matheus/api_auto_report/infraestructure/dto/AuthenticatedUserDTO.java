54b69135e0352a48c92673a8b1f9db3df75b3f36
Matheus Garibaldi <matheus.garibaldi8@gmail.com>
2025-09-19 14:53:53 -0300

    feat: add userLogin and AuthenticatedUser DTOs

---
+++ src/main/java/com/matheus/api_auto_report/infraestructure/dto/AuthenticatedUserDTO.java
@@ -0,0 +1,4 @@
+package com.matheus.api_auto_report.infraestructure.dto;
+
+public record AuthenticatedUserDTO(Long userId, String username) {
+}
---
+++ src/main/java/com/matheus/api_auto_report/infraestructure/dto/UserLoginDTO.java
@@ -0,0 +1,4 @@
+package com.matheus.api_auto_report.infraestructure.dto;
+
+public record UserLoginDTO(String email, String password) {
+}
